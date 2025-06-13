69826548f9fb51d195f96ebba97085d5d7232f92
BrunoLc <bcasavalle@gmail.com>
2025-06-13 01:34:43 -0300

    Realizamos el Motor de Busqueda de Productos

--- TP/61072 - Casavalle Castillo, Bruno/tp6/cliente/Program.cs
+++ TP/61072 - Casavalle Castillo, Bruno/tp6/cliente/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<CarritoService>();
-
+builder.Services.AddSingleton<BusquedaService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5184/") });
 builder.Services.AddScoped<ProductoService>();
 
