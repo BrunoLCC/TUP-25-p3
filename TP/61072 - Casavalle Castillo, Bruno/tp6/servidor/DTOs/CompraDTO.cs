b5b7c0a4693a5cc8a1b5fd303b58d823db2a3bb3
BrunoLc <bcasavalle@gmail.com>
2025-06-14 15:18:37 -0300

    Realizamos Validaciones del Formulario

--- TP/61072 - Casavalle Castillo, Bruno/tp6/servidor/DTOs/CompraDTO.cs
+++ TP/61072 - Casavalle Castillo, Bruno/tp6/servidor/DTOs/CompraDTO.cs
@@ -1,16 +1,32 @@
-namespace servidor.DTOs;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-public class CompraDto
+namespace servidor.DTOs
 {
-    public string NombreCliente { get; set; } = string.Empty;
-    public string ApellidoCliente { get; set; } = string.Empty;
-    public string EmailCliente { get; set; } = string.Empty;
+    public class CompraDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string NombreCliente { get; set; } = string.Empty;
 
-    public List<ItemCompraDto> Items { get; set; } = new();
-}
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        public string ApellidoCliente { get; set; } = string.Empty;
 
-public class ItemCompraDto
-{
-    public int ProductoId { get; set; }
-    public int Cantidad { get; set; }
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "Email no válido")]
+        public string EmailCliente { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Debe incluir al menos un producto en la compra")]
+        public List<ItemCompraDto> Items { get; set; } = new();
+    }
+
+    public class ItemCompraDto
+    {
+        [Required]
+        public int ProductoId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1")]
+        public int Cantidad { get; set; }
+    }
 }
